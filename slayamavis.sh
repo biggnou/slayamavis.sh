cf886fa509463100606cadbbc6d6000b0a196641
Alexandre Boyer <biggnou@gmail.com>
2012-05-30 19:18:14 -0400

    Added the script.

---
+++ slayamavis.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# @author: ajb@zerospam.ca
+# @purpose: copy working dir of amavis, slay user amavis, start amavis, when amavis is in pain.
+
+# check for root privileges
+if [ "$(id -u)" != "0" ]; then
+    echo "You must be root in order to use this script."
+    exit 1
+fi
+
+FILENAME=amavistemp.$(date +%d%m%Y-%s)
+
+ps auxwwf | egrep '^amavis'
+cp -r /var/lib/amavis/tmp/ /home/${SUDO_USER}/${FILENAME} && chown ${SUDO_USER}:${SUDO_USER} /home/${SUDO_USER}/${FILENAME}
+slay amavis && service amavis start
+sleep 3
+ps auxwwf | egrep '^amavis'
